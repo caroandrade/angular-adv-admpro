f7a73a8bd63a2683fe38f0b763e2d1995941afb2
Carolina Andrade <andrade.carito@gmail.com>
2022-07-26 12:44:42 -0300

    fin seccion-router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,29 @@
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes} from '@angular/router'
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { pagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
 
 
 const routes: Routes=[
-  { path:'',
   
-  component: PagesComponent,
-  children:[ { path: 'dashboard', component: DashboardComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'grafica1', component: Grafica1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch:'full' },
-]
-},
  
- 
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
- 
+ //paht:'/dashbiard' PagesRouting
+ //paht 'auth' AutRouting
+  
+ { path: '', redirectTo: '/dashboard', pathMatch:'full' },
   {path: '**', component: NopagefoundComponent}
 ]
 
 @NgModule({
-  declarations: [],
+ 
   imports: [ 
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    pagesRoutingModule,
+    AuthRoutingModule
   ],
   exports:[
     RouterModule
